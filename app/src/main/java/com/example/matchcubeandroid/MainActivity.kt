72075777bd56179bf155cb55ee91fae684098293
juntaepark <jt0015@naver.com>
2021-02-15 01:08:49 +0900

    fragment 설계

--- app/src/main/java/com/example/matchcubeandroid/MainActivity.kt
+++ app/src/main/java/com/example/matchcubeandroid/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.matchcubeandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        match.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmnetview, MatchFragment())
+                    .commit()
+        }
+        messenger.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmnetview, MessengerFragment())
+                    .commit()
+        }
+        live.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmnetview, LiveFragment())
+                    .commit()
+        }
+        mypage.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmnetview, MyPageFragment())
+                    .commit()
+        }
     }
 
 
