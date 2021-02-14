fd3d334f50a86c4143d95d667c64909e935e281f
juntaepark <jt0015@naver.com>
2021-02-15 01:08:49 +0900

    fragment 설계

---
+++ app/src/main/java/com/example/matchcubeandroid/MessengerFragment.kt
@@ -0,0 +1,14 @@
+package com.example.matchcubeandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MessengerFragment : Fragment()  {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_messenger, container, false)
+    }
+}
