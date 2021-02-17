cc08a1ad4a1182970bfa955e4579dde545c56ea6
JihoKevin <jhshin925@gmail.com>
2021-02-17 16:03:44 +0900

    Revert "added login, register activity"
    This reverts commit 8224a2513aaee44d686e944314c728ef28c1dc61.

--- app/src/main/java/com/example/matchcubeandroid/login/LoginActivity.kt
+++ app/src/main/java/com/example/matchcubeandroid/login/LoginActivity.kt
@@ -2,11 +2,37 @@ package com.example.matchcubeandroid.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.matchcubeandroid.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private var btnLogin: Button? = null
+    private var btnRegister: Button? = null
+    private var editId: EditText? = null
+    private var editPw: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnRegister = findViewById<Button>(R.id.btnRegister)
+        editId = findViewById<EditText>(R.id.editId)
+        editPw = findViewById<EditText>(R.id.editPw)
+
+        btnLogin?.setOnClickListener (object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                // TODO
+            }
+        })
+
+        btnRegister?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                // TODO
+            }
+        })
     }
 }
--- app/src/main/java/com/example/matchcubeandroid/register/RegisterActivity.kt
+++ app/src/main/java/com/example/matchcubeandroid/register/RegisterActivity.kt
@@ -2,11 +2,28 @@ package com.example.matchcubeandroid.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.matchcubeandroid.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var btnRegister: Button? = null // 회원가입 완료 버튼 선언
+    private var editEmail: EditText? = null // 이메일 입력 EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnRegister = findViewById<Button>(R.id.btnRegister) // 회원가입 버튼 클릭 이벤트 리스너 ID 찾아오기
+
+        btnRegister?.setOnClickListener(object : View.OnClickListener{ // 회원 가입 버튼 클릭 이벤트 리스너
+            override fun onClick(v: View?) {
+                // TODO
+            }
+        })
+
+
     }
 }
