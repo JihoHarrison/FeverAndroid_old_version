0977ad25814bdcf7625c4e5668869e2af7a4170f
JihoKevin <jhshin925@gmail.com>
2021-02-17 15:07:00 +0900

    Revert "added login, register activity"
    This reverts commit 8224a2513aaee44d686e944314c728ef28c1dc61.

---
+++ app/src/main/java/com/example/matchcubeandroid/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.matchcubeandroid.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.matchcubeandroid.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
