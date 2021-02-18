f6f753c941016a321cf338cb1c6b4c376c159fd9
juntaepark <jt0015@naver.com>
2021-02-18 11:36:29 +0900

    Lamda, DataBinding

--- app/src/main/java/com/example/matchcubeandroid/activities/login/LoginActivity.kt
+++ app/src/main/java/com/example/matchcubeandroid/activities/login/LoginActivity.kt
@@ -1,16 +1,19 @@
 package com.example.matchcubeandroid.activities.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.example.matchcubeandroid.R
+import com.example.matchcubeandroid.activities.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     private var btnLogin: Button? = null
-    private var btnRegister: Button? = null
+    //private var btnRegister: Button? = null
     private var editId: EditText? = null
     private var editPw: EditText? = null
 
@@ -19,7 +22,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnRegister = findViewById<Button>(R.id.btnRegister)
+        //btnRegister = findViewById<Button>(R.id.btnRegister)
         editId = findViewById<EditText>(R.id.editId)
         editPw = findViewById<EditText>(R.id.editPw)
 
@@ -29,10 +32,15 @@ class LoginActivity : AppCompatActivity() {
             }
         })
 
-        btnRegister?.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                // TODO
-            }
-        })
+        btnRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+//        btnRegister?.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                 TODO
+//            }
+//        })
     }
 }
