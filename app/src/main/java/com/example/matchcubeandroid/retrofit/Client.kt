a60e467f72caa3b78c0885511db87cdd84584e22
JihoKevin <jhshin925@gmail.com>
2021-02-24 10:20:34 +0900

    Client.kt에서 retrofit 변수에 baseUrl 주소를 상수로 설정 . . .

--- app/src/main/java/com/example/matchcubeandroid/retrofit/Client.kt
+++ app/src/main/java/com/example/matchcubeandroid/retrofit/Client.kt
@@ -7,7 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object Client {
-    var retrofitService: API
+    private var retrofitService: API
+    private val BASE_URL: String = "http://10.0.2.2:8080/"
 
     init{
         val interceptor = HttpLoggingInterceptor()
@@ -15,7 +16,7 @@ object Client {
         val logger = OkHttpClient.Builder().addInterceptor(interceptor).readTimeout(20, TimeUnit.SECONDS).writeTimeout(20, TimeUnit.SECONDS).build()
 //10.0.2.2
         val retrofit = Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(logger)
                 .build()
