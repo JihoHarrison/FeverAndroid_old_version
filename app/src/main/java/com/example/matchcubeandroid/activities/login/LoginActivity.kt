382b7089a32eb71f17f6f22e94ac7c79ed4c4168
JihoKevin <jhshin925@gmail.com>
2021-03-06 14:12:44 +0900

    구글로그인 완성 (에뮬레이터에서는 되는데 기기에서 테스트 해봐야함) 오류나면 다시 고쳐야함!

--- app/src/main/java/com/example/matchcubeandroid/activities/login/LoginActivity.kt
+++ app/src/main/java/com/example/matchcubeandroid/activities/login/LoginActivity.kt
@@ -1,27 +1,45 @@
 package com.example.matchcubeandroid.activities.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.matchcubeandroid.R
 import com.example.matchcubeandroid.activities.main.MainActivity
 import com.example.matchcubeandroid.activities.register.RegisterActivity
 import com.example.matchcubeandroid.model.LogInModel
 import com.example.matchcubeandroid.retrofit.Client
 import com.example.matchcubeandroid.sharedPreferences.MySharedPreferences
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
+import com.kakao.sdk.common.model.AuthErrorCause.*
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.*
 import kotlin.Result.Companion.success
-import com.kakao.sdk.common.model.AuthErrorCause.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     private val TAG = "retrofit"
+    private lateinit var auth: FirebaseAuth
+    lateinit var authListener : FirebaseAuth.AuthStateListener
+    //google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private val RC_SIGN_IN = 200
+
 
    // kakao
     val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
@@ -63,9 +81,85 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun signIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun updateUI(account: FirebaseUser?) {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // 로그인 한 기록이 있는지 확인
+
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = auth.currentUser
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        // ..
+                        updateUI(null)
+                    }
+
+                    // ...
+                }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("801343645302-nhv012aj5go93vujgcl3pqmvt5utucbs.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+
+        btnGoogleLogin.setOnClickListener{
+            // Configure Google Sign In
+            signIn()
+
+        }
+
+
 
         btnKakaoLogin.setOnClickListener {
             if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
@@ -77,40 +171,39 @@ class LoginActivity : AppCompatActivity() {
 
         btnLogin.setOnClickListener {
             val body = HashMap<String, String>()
-            body.put("emailId", editId.text.toString() )
-            body.put("password", editPw.text.toString() )
+            body.put("emailId", editId.text.toString())
+            body.put("password", editPw.text.toString())
             // 서비스 호출부
             Client.retrofitService.logIn(body).enqueue(object : Callback<LogInModel> {
-                    override fun onResponse(call: Call<LogInModel>, response: Response<LogInModel>) {
+                override fun onResponse(call: Call<LogInModel>, response: Response<LogInModel>) {
 
-                        if (response.body()?.statusCode == 200){ // 200 : successful
-                            val data = response.body()?.data
-                            data?.let { success(data) }
-                            // 자동 로그인 처리
-                            if ( checkAutoLogin.isChecked()) {
-                                MySharedPreferences.setAutoChecked(this@LoginActivity, "Y")
-                            }
-                            else{
-                                MySharedPreferences.setAutoChecked(this@LoginActivity, "N")
-                            }
-                            // 데이터 저장
-                            MySharedPreferences.setUserId(this@LoginActivity, editId.text.toString())
-                            MySharedPreferences.setUserPass(this@LoginActivity, editPw.text.toString())
-
-                            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-
-                        } else{
-                            Toast.makeText(this@LoginActivity, response.body()?.responseMessage, Toast.LENGTH_SHORT).show()
-                            Log.d(TAG, "${response.body()?.toString()}")
+                    if (response.body()?.statusCode == 200) { // 200 : successful
+                        val data = response.body()?.data
+                        data?.let { success(data) }
+                        // 자동 로그인 처리
+                        if (checkAutoLogin.isChecked()) {
+                            MySharedPreferences.setAutoChecked(this@LoginActivity, "Y")
+                        } else {
+                            MySharedPreferences.setAutoChecked(this@LoginActivity, "N")
                         }
-                    }
+                        // 데이터 저장
+                        MySharedPreferences.setUserId(this@LoginActivity, editId.text.toString())
+                        MySharedPreferences.setUserPass(this@LoginActivity, editPw.text.toString())
 
-                    override fun onFailure(call: Call<LogInModel>?, t: Throwable?) {
-                        t?.message?.let {
-                            Toast.makeText(this@LoginActivity, it, Toast.LENGTH_SHORT).show()
-                        }
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+
+                    } else {
+                        Toast.makeText(this@LoginActivity, response.body()?.responseMessage, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, "${response.body()?.toString()}")
                     }
-                })
+                }
+
+                override fun onFailure(call: Call<LogInModel>?, t: Throwable?) {
+                    t?.message?.let {
+                        Toast.makeText(this@LoginActivity, it, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
         }
 
         btnRegister.setOnClickListener{
