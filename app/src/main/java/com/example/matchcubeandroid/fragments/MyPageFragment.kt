5f0f41c763358643de86ba6adc15c4e042c4bc82
Corphy <kmj9706210327@gmail.com>
2021-05-16 19:10:41 +0900

    마이페이지 수정

--- app/src/main/java/com/example/matchcubeandroid/fragments/MyPageFragment.kt
+++ app/src/main/java/com/example/matchcubeandroid/fragments/MyPageFragment.kt
@@ -37,11 +37,6 @@ class MyPageFragment : Fragment() {
         val view: View = inflater!!.inflate(R.layout.fragment_my_page, container, false)
         var context: Context = view.context
         val name: TextView = view.findViewById(R.id.txtName)
-        val nickname: TextView = view.findViewById(R.id.txtNickName)
-        val email: TextView = view.findViewById(R.id.txtEmail)
-        val editname: EditText = view.findViewById(R.id.editName)
-        val editnickname: EditText = view.findViewById(R.id.editNickName)
-        val editemail: EditText = view.findViewById(R.id.editEmailAddress)
         val profileimage: CircleImageView = view.findViewById(R.id.imgProfile)
 
 
@@ -54,8 +49,6 @@ class MyPageFragment : Fragment() {
                     Toast.makeText(context, response.body()?.responseMessage, Toast.LENGTH_SHORT).show()
                     Log.d("Account", "${response.body()?.toString()}")
                     name.setText(data?.name)
-                    nickname.setText(data?.nickName)
-                    email.setText(data?.emailId)
 
                     // 이미지 처리 객체
                     var image_task: URLtoBitmapTask = URLtoBitmapTask()
@@ -107,41 +100,16 @@ class MyPageFragment : Fragment() {
 
 
 
-        val switcher = view.findViewById(R.id.my_switcher) as ViewSwitcher
 
-        val btn: Button = view.findViewById(R.id.btnEditMyProfile)
-        val btn2: Button = view.findViewById(R.id.btnEditMyProfileComplete)
+        val btn: TextView = view.findViewById(R.id.btnEditMyProfile)
         val rvTeamProfImgs: RecyclerView = view.findViewById(R.id.rvTeamProfImgs)
         val opsList: ListView = view.findViewById(R.id.options)
         btn.setOnClickListener {
-            name.visibility = View.INVISIBLE
-            editname.visibility = View.VISIBLE
-            nickname.visibility = View.INVISIBLE
-            editnickname.visibility = View.VISIBLE
-            email.visibility = View.INVISIBLE
-            editemail.visibility = View.VISIBLE
-
-            switcher.showNext() //or switcher.showPrevious();
-
-
 
 
         }
 
-        btn2.setOnClickListener{
-            name.visibility = View.VISIBLE
-            editname.visibility = View.INVISIBLE
-            nickname.visibility = View.VISIBLE
-            editnickname.visibility = View.INVISIBLE
-            email.visibility = View.VISIBLE
-            editemail.visibility = View.INVISIBLE
-            var newName: String = editname.text.toString()
-            name.setText(newName)
 
-
-
-            switcher.showPrevious()
-        }
 
 
         val profileList = arrayListOf( // DB에 저장된 값으로 수정 가능 할 것으로 보임!
